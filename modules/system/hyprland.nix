9454eb1b5c40c8e899022d1e2c676678138f6a77
Pohlrabi <pohlrabi404@gmail.com>
2024-09-24 22:42:02 +0900

    provide other packages

--- modules/system/hyprland.nix
+++ modules/system/hyprland.nix
@@ -3,4 +3,35 @@
 
   programs.hyprland.enable = true;
 
+  environment.systemPackages = with pkgs; [
+    (waybar.overrideAttrs (oldAttrs: {
+      mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+    }))
+    dunst
+    libnotify
+
+    #Wallpaper
+    swww
+
+    #Terminal
+    kitty
+
+    #App Launcher
+    rofi-wayland
+
+  ];
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  
+  sound.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
 }
